7fa8fc44f8ca33e02a95f7bca00e756fe25ced91
avery-mclocklin1 <averywmclocklin@gmail.com>
2020-12-03 11:46:52 -0500

    Second gold star! Day 01 part 2

---
+++ 2020/day01/src/expense_sum_pt2.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define EXPENSE_REPORT "input"
+
+/* Returns number of lines in report */
+int get_report_length(FILE *fp)
+{
+	int count = 0;
+	char c;
+
+	rewind(fp);
+	for (c = getc(fp); c != EOF; c = getc(fp)) {
+		if (c == '\n') {
+			count = count + 1;
+		}
+	}
+
+	return count;
+}
+
+/* Reads each line of the report, cast types to integer in array */
+void read_report(FILE *fp, int *rep)
+{
+	char buff[10];
+
+	rewind(fp);
+	for( int i = 0; fgets(buff, 10, fp) != NULL; i++ ) {
+		rep[i] = atoi(buff);
+	}
+}
+
+/* Search and compare up array until element tri-pair equals 2020.
+ * Return multiplication of the three elements.
+ */
+int find_tri_pair(int *rep, int f_len)
+{
+	for (int i = 0; i < f_len-2; i++) {
+		for (int j = i+1; j < f_len-1; j++) {
+			for (int k = j+1; k < f_len; k++) {
+				if (rep[i]+rep[j]+rep[k] == 2020) {
+					return rep[i] * rep[j] * rep[k];
+				}
+			}
+		}
+	}
+
+	return 0;
+}
+
+int main(void)
+{
+	FILE *report;
+	int f_len;
+	int *rep_array;
+	int pair_multi;
+
+	report = fopen(EXPENSE_REPORT, "r");
+
+	if (report == NULL) {
+		printf("Could not open file %s\n", EXPENSE_REPORT);
+		return 1;
+	}
+
+	f_len = get_report_length(report);
+	rep_array = (int*)calloc(f_len, sizeof(int));
+	read_report(report, rep_array);
+	pair_multi = find_tri_pair(rep_array, f_len);
+
+	if ( pair_multi )
+		printf("Found tri-pair multiplication: %i\n", pair_multi);
+	else 
+		printf("Failed to accurately find tri-pair\n");
+
+	free(rep_array);
+	fclose(report);
+
+	return 0;
+}
