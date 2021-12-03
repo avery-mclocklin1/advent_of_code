bf6550cda399d8d8ed5b14648fbf213bba4076d4
avery-mclocklin1 <averywmclocklin@gmail.com>
2021-12-03 09:52:35 -0500

    Solutions for day02
    Both parts loop over the key-value pairs, sorting by key to determine
    which action to take.

---
+++ 2021/day02/src/part1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../../lib/inc/parseinput.h"
+
+#define INPUT "/home/avery/misc/advent_of_code/2021/inputs/day02"
+
+int main(void) {
+	FILE *report;
+	int f_len;
+	int depth = 0, hzl = 0; // Depth and Horizontal coordinates.
+	keyValueInt_t **kv_array;
+
+	report = fopen(INPUT, "r");
+	f_len = get_report_length(report);
+
+	kv_array = (keyValueInt_t**)calloc(f_len, sizeof(keyValueInt_t*));
+	
+	read_report_kv_int(report, kv_array);
+
+	for (int i = 0; i < f_len; i++) {
+		if (!strcmp(kv_array[i]->key, "up")) {
+			depth -= kv_array[i]->value;
+		}
+		else if(!strcmp(kv_array[i]->key, "down")) {
+			depth += kv_array[i]->value;
+		}
+		else if(!strcmp(kv_array[i]->key, "forward")) {
+			hzl += kv_array[i]->value;
+		}
+
+		free(kv_array[i]);
+	}
+
+	printf("Multiplied Horizontal and Depth: %i\n", depth * hzl);
+
+	/* Clean up */
+	free(kv_array);
+
+	return 0;
+}
